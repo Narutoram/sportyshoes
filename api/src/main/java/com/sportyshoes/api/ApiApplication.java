a0a427636fc98222fdb3659cee3f6cd2538f65d8
Narutoram <gohanram15@gmail.com>
2022-05-12 09:54:47 +0530

    with swagger

--- api/src/main/java/com/sportyshoes/api/ApiApplication.java
+++ api/src/main/java/com/sportyshoes/api/ApiApplication.java
@@ -4,26 +4,17 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication()
-//@EnableSwagger2
+@EnableSwagger2
 public class ApiApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(ApiApplication.class, args);
 	}
 	
-//	@Bean
-//	public Docket swaggerConfiguration() {
-//		return new Docket(DocumentationType.SWAGGER_2).select()
-//				.apis(RequestHandlerSelectors.basePackage("com.sportyshoes.api.controller"))
-//				.build()
-//				.apiInfo(apiDetails());
-//	}
-//	private ApiInfo apiDetails() {
-//		return new ApiInfoBuilder().title("Sping Boot Sportyshoes project")
-//				.description("Spring Boot sportyshoes app with Swagger").version("1.0").termsOfServiceUrl("No terms")
-//				.license("No license")
-//				.contact(new Contact("Naruto", "https://github.com/Narutoram", "abc@example.com")).build();
-//	}
-	
+	@Bean
+	public Docket productApi() {
+		return new Docket(DocumentationType.SWAGGER_2).select()
+				.apis(RequestHandlerSelectors.basePackage("com.sportyshoes.api")).build();
+	}
 
 }
